2c5977967287e239137dcfad36d176c03a893642
OGKosjusz <dawkwi4@gmail.com>
2025-10-27 14:10:43 +0100

    27.10.2025

---
+++ 1-Pierwszy program C++/1-Struktura programu C++.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Witaj w programowaniu C++" ;
+	return 0;
+}
---
+++ 1-Pierwszy program C++/2-Zmienna.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a, b;
+	cout << "Liczba a: ";
+	cin >> a;
+	cout << "Liczba b: ";
+	cin >> b;
+	
+	cout << "Suma: " << a + b << endl;
+	cout << "Roznica: " << a - b << endl;
+	cout << "Iloczyn: " << a * b << endl;
+	return 0;
+}
